7d68707e41591cfc0bce3c40546a9d35bbf9afd9
AndersonVargas8 <anvargasa@unal.edu.co>
2021-11-03 12:29:05 -0500

    Se creó el template de la ventana principal

---
+++ src/VentanaPrincipalTemplate.java
@@ -0,0 +1,14 @@
+public class VentanaPrincipalTemplate{
+	private VentanaPrincipalTemplate ventanaP;
+	private VentanaPrincipalTemplate(){
+		
+	}
+
+	public getVentanaP(){
+		if(ventanaP == null)
+			ventanaP = new VentanaPrincipalTemplate;
+
+		return ventanaP;
+	}
+
+}
